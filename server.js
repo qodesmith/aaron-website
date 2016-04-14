3e57ac1333c925ef0f0f3a8f9d23455b2fe70012
qodesmith <theqodesmith@gmail.com>
2016-04-14 09:16:05 -0400

    Revised server.

--- server.js
+++ server.js
@@ -12,19 +12,45 @@ var app = express();
 app.use(bp.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-function sendData(req, path) {
-  // Send the JavaScript & styles. Let the front-end worry about what to render.
-  var meta = '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
-  var styles = '<link rel="stylesheet" href="/styles.css">';
-  var scripts = '<script src="/all.min.js"></script>';
-  var homepage = meta + '<body class="sans full-size">' + styles + scripts + '</body>';
+// Send the JavaScript & styles. Let the front-end worry about what to render.
+var meta = '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+var styles = '<link rel="stylesheet" href="/styles.css">';
+var scripts = '<script src="/all.min.js"></script>';
+var homepage = meta + '<body class="sans full-size">' + styles + scripts + '</body>';
 
-  return homepage;
-}
+// app.get('/about', function(req, res) {
+//   console.log(req.url + ' route');
+//   res.send(homepage);
+// });
+
+// app.get('/projects', function(req, res) {
+//   console.log(req.url + ' route');
+//   res.send(homepage);
+// });
+
+// app.get('/projects/:id', function(req, res) {
+//   console.log(req.url + ' route');
+//   res.send(homepage);
+// });
+
+// app.get('/regular-resume', function(req, res) {
+//   console.log(req.url + ' route');
+//   res.send(homepage);
+// });
+
+// app.get('/nerdy-resume', function(req, res) {
+//   console.log(req.url + ' route');
+//   res.send(homepage);
+// });
+
+// app.get('/contact', function(req, res) {
+//   console.log(req.url + ' route');
+//   res.send(homepage);
+// });
 
 app.get('*', function(req, res) {
-  console.log("Catch all 'get' route");
-  res.send(sendData(req));
+  console.log('Catch all route');
+  res.send(homepage);
 });
 
 app.post('/contact', function(req, res) {
